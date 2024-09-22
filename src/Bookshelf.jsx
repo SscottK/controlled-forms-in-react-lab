749f23d6ed9a8d024e366546893bdd67f45df22d
ScottKinnear <gr8whtninjassk@gmail.com>
2024-09-22 15:03:25 -0700

    Full feture functionality

---
+++ src/Bookshelf.jsx
@@ -0,0 +1,53 @@
+import { useDebugValue, useState } from "react";
+
+const Bookshelf = () => {
+    const [books, setBooks] = useState([]);
+    const [newBook, setNewBook] = useState({
+        title: '',
+        author: ''
+    });
+
+    const handleInputChange = (event) => {
+        
+        setNewBook({
+            ...newBook, 
+            [event.target.name]: event.target.value
+        })
+
+    };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBooks([...books, newBook])
+    }
+
+
+    return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add A Book</h3>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="title">Title:</label>
+                    <input name="title" 
+                    onChange={handleInputChange} 
+                    value={newBook.title}/>
+                    <label htmlFor="author">Author: </label>
+                    <input name="author" onChange={handleInputChange} value={newBook.author}/>
+                    <button type="submit" >Add Book</button>
+                </form>
+            </div>
+            <div className="bookCardDiv">
+                {books.map((book) => {
+                    return (
+                        <div className="bookCard">
+                            <h3>{book.title}</h3>
+                            <p>{book.author}</p> 
+                        </div>                      
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Bookshelf;
